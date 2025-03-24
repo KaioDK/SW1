24aa402dc595496892903905546d9c60f3b47634
Kaio Lima <129626493+KaioDK@users.noreply.github.com>
2025-03-23 21:31:22 -0300

    Update index.php

--- Aula_05/index.php
+++ Aula_05/index.php
@@ -12,23 +12,23 @@
         <form action="processa.php" method="POST">
             <div data-mdb-input-init class="form-outline mb-4">
                 <input type="email" id="form1Example1" class="form-control" name="email" />
-                <label class="form-label" for="form1Example1">Email address</label>
+                <label class="form-label" for="form1Example1">Endereço de email</label>
             </div>
 
             <div data-mdb-input-init class="form-outline mb-4">
                 <input type="password" id="form1Example2" class="form-control" name="senha" />
-                <label class="form-label" for="form1Example2">Password</label>
+                <label class="form-label" for="form1Example2">Senha</label>
             </div>
 
             <div class="row mb-4">
                 <div class="col d-flex justify-content-center">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-                        <label class="form-check-label" for="form1Example3"> Remember me </label>
+                        <label class="form-check-label" for="form1Example3"> Lembre de mim </label>
                     </div>
                 </div>
                 <div class="col">
-                    <a href="#!">Forgot password?</a>
+                    <a href="#!">Esqueci a senha?</a>
                 </div>
             </div>
         
